6f0897cea81fb1f9d10488aa82b212d1cfb575b5
Frederic Onana <onanafrederic31@gmail.com>
2024-12-03 22:55:08 +0100

    yo

--- Frontend/croptop/src/app/app.routes.ts
+++ Frontend/croptop/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../app/Pages/login/login.component';
 import { SignupComponent } from '../app/Pages/signup/signup.component';
 import { LandingpageComponent } from '../app/Pages/landingpage/landingpage.component';
+import { FarmsComponent } from './Pages/farms/farms.component';
 
 export const routes: Routes = [
   { path: '', component: LandingpageComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'farm', component: FarmsComponent}
+
 ];
