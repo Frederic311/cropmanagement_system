97dc44d66efcc178cc95652832a53b4e7a4afe1e
Allabira-Abdoul <allabiracharafaddine@gmail.com>
2024-12-03 15:02:29 +0100

    Backend initialised

---
+++ Backend/CropManagement/src/main/java/com/group2/cropmanagement/CropManagementApplication.java
@@ -0,0 +1,13 @@
+package com.group2.cropmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropManagementApplication.class, args);
+    }
+
+}
