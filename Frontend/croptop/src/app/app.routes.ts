2692e3212afa9ca71643ad846a4d9ecd38725775
Valere Nz. <nz-valere>
2024-12-05 20:10:28 +0100

    crop page implementation

--- Frontend/croptop/src/app/app.routes.ts
+++ Frontend/croptop/src/app/app.routes.ts
@@ -4,12 +4,23 @@ import { SignupComponent } from '../app/Pages/signup/signup.component';
 import { LandingpageComponent } from '../app/Pages/landingpage/landingpage.component';
 // import { FarmsComponent } from './Pages/farms/farms.component';
 import { LeftSidebarComponent } from './features/left-sidebar/left-sidebar.component';
+import { TestComponent } from './Pages/test/test.component';
+import { CropPageComponent } from './Pages/crop-page/crop-page.component';
 
 export const routes: Routes = [
   { path: '', component: LandingpageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'sidebar', component: LeftSidebarComponent },
+  {
+    path: 'crop-page', 
+    component: CropPageComponent
+  },
+  { 
+    path: 'test', 
+    component: TestComponent
+  },
+
 
   // { path: 'farm', component: FarmsComponent}
 
---
+++ Frontend/croptop/src/app/features/crop-card/crop-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crop-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './crop-card.component.html',
+  styleUrl: './crop-card.component.css'
+})
+export class CropCardComponent {
+
+}
